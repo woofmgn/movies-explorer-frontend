374d6e6f5074d8afa78a099b3163ac4e65e8762b
Denis Vakulenko <woof88@yandex.ru>
2022-11-30 12:25:44 +0500

    feat: added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AboutProject from './components/AboutProject/AboutProject';
+import Header from './components/Header/Header';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Movies from './pages/Movies/Movies';
+import Profile from './pages/Profile/Profile';
+import Register from './pages/Register/Register';
+import SavedMovies from './pages/SavedMovies/SavedMovies';
 
 function App() {
   return (
-    <div className="App">
-    
-
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about-project" element={<AboutProject />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
